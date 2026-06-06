6ee8544f903f44294017a2b343431d2bb077c8e4
pidin <pidinplusplusultra@gmail.com>
2026-06-06 15:11:20 +0900

    fix small stuff

--- src/supabase-config.js
+++ src/supabase-config.js
@@ -1,4 +1,4 @@
 window.CANOPY_FOCUS_SUPABASE = {
-  url: "https://fhmbrgvibplbhysxmbnd.supabase.co/rest/v1/",
+  url: "https://fhmbrgvibplbhysxmbnd.supabase.co",
   anonKey: "sb_publishable_IxLdtSF3dEBQwOkm5yrcWg_Zsr-nxIg",
 };
